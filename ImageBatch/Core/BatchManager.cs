7ca580c28efe6fa7d657abed640b642e7baf0ff7
GabrieleNunez <gabrieleNunez@thecoconutcoder.com>
2014-05-17 04:53:39 -0400

    -Scanner class created and implemented     - This is what picks up and finds the files.     - An event will be triggered when ever a file is found with valid extension -BatchManager class created. NOT implemented

---
+++ ImageBatch/Core/BatchManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageBatch.Core
+{
+    public class BatchManager
+    {
+        private Stack<string> files;
+    }
+}
---
+++ ImageBatch/Core/Scanner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ImageBatch.Core
+{
+    public delegate void ScannerEventHandler(string file);
+
+    /// <summary>
+    /// Scanner will enumurate over files in the given directory.
+    /// As it scans any file with valid extension will be trigger a ImageFound event
+    /// </summary>
+    public class Scanner
+    {
+        private string directory;
+        private SearchOption searchOption;
+        public event ScannerEventHandler ImageFound;
+
+        public Scanner(string Directory,SearchOption SearchOption)
+        {
+            directory = Directory;
+            searchOption = SearchOption;
+        }
+
+        /// <summary>
+        /// Start scanning and enumurating picking up any files with valid extensions
+        /// </summary>
+        public void Scan()
+        {
+            IEnumerable<string> files = Directory.EnumerateFiles(directory, "*.*", searchOption);
+            foreach (string path in files)
+            {
+                switch (Path.GetExtension(path).ToLower())
+                {
+                    case ".jpg":
+                    case ".jpeg":
+                    case ".png":
+                    case ".gif":
+                        if (ImageFound != null)
+                        {
+                            ImageFound.Invoke(path);
+                            continue;
+                        }
+                        continue;
+                    default:
+                        continue;
+                }
+            }
+        }
+    }
+}
